caaa3e3e2e3c5030d4eb375c3dfdd210f4803b34
MyUsername <rosanio.c@gmail.com>
2025-04-29 11:29:09 +0200

    NOT FOUND - Stiizzazione pagina

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,26 @@
+// UTILITY
+import { useNavigate } from "react-router-dom";
+
+
+// COMPONENTS
+import RoundButton from "../components/RoundButton"
+
+
 // COMPONENT EXPORT
 export default function NotFoundPage() {
+
+    // NAVIGATE
+    const navigate = useNavigate();
+
     return <>
 
-        Page not found
+        <h1>Page not found</h1>
+        <div className='detailsSection'>
+            <button
+                className='sortButton'
+                onClick={() => navigate(-1)}
+            >BACK ➜</button>
+        </div>
 
     </>
 }
